8fe2f81b8930183ddf9174121f4db25adda54145
Unknown <sodomyoindy@gmail.com>
2018-01-22 18:23:59 +0700

    เสร็จแล้ว

--- lab_7_4.cpp
+++ lab_7_4.cpp
@@ -5,41 +5,21 @@ using namespace std;
 int main () {
 
   // Without loop
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
-  cout << "*" << endl;
-  cout << "**" << endl;
-  cout << "***" << endl;
-  cout << "****" << endl;
-  cout << "*****" << endl;
-  cout << "******" << endl;
-  cout << "*****" << endl;
-  cout << "****" << endl;
-  cout << "***" << endl;
-  cout << "**" << endl;
-  cout << "*" << endl;
 
   // เขียนโปรแกรมให้ทำงานได้ตามข้างต้นโดยใช้ Loop
+  for ( int y = 0; y < 3; y++){
+     cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "***" << endl;
+  cout << "****" << endl;
+  cout << "*****" << endl;
+  cout << "******" << endl;
+  cout << "*****" << endl;
+  cout << "****" << endl;
+  cout << "***" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  }
 
 
   return 0;
