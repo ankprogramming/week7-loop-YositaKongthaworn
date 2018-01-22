13a73ca1ba0e672a7a6c0160e97f9af8c1ca009d
Unknown <sodomyoindy@gmail.com>
2018-01-22 18:24:18 +0700

    เสร็จแล้ว

--- lab_7_7.cpp
+++ lab_7_7.cpp
@@ -5,6 +5,18 @@ using namespace std;
 int main () {
 
   // เขียนโปรแกรมเพื่อรับค่าตัวเลข 5 จำนวน และหาค่าเฉลี่ย
+  int y;
+  int sum=0;
+  float avg;
+
+  for (int i=0; i<=5;){
+
+  cin >> y;
+  sum += y;
+  }
+
+  avg = (float)sum / 5;
+  cout <<avg <<endl;
 
   return 0;
 }
